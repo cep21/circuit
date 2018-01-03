f335cebde151f8ca431cb3551511931a56e2e6f5
Jack Lindamood <cep221@gmail.com>
2018-01-02 20:55:50 -0800

    Fix lints

--- closers/hystrix/config.go
+++ closers/hystrix/config.go
@@ -2,13 +2,14 @@ package circuit
 
 import "github.com/cep21/circuit"
 
+// ConfigFactory aids making hystrix circuit logic
 type ConfigFactory struct {
 	ConfigureSleepyCloseCheck    ConfigureSleepyCloseCheck
 	ConfigureOpenOnErrPercentage ConfigureOpenOnErrPercentage
 }
 
 // Configure creates a circuit configuration constructor that uses hystrix open/close logic
-func (c *ConfigFactory) Configure(circuitName string) circuit.Config {
+func (c *ConfigFactory) Configure(_ string) circuit.Config {
 	return circuit.Config{
 		General: circuit.GeneralConfig{
 			OpenToClosedFactory: SleepyCloseCheckFactory(c.ConfigureSleepyCloseCheck),
