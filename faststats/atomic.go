7c6ff6d4ed57c6f5728f36ba9fbaf4dde13a207b
Jack Lindamood <jack@cresta.ai>
2024-01-09 11:09:05 -0800

    feat: use Go built in atomic package

--- faststats/atomic.go
+++ faststats/atomic.go
@@ -8,20 +8,18 @@ import (
 )
 
 // AtomicBoolean is a helper struct to simulate atomic operations on a boolean
-type AtomicBoolean struct{ flag uint32 }
+type AtomicBoolean struct {
+	atomic.Bool
+}
 
 // Get the current atomic value
 func (a *AtomicBoolean) Get() bool {
-	return atomic.LoadUint32(&a.flag) == 1
+	return a.Load()
 }
 
 // Set the atomic boolean value
 func (a *AtomicBoolean) Set(value bool) {
-	if value {
-		atomic.StoreUint32(&a.flag, 1)
-	} else {
-		atomic.StoreUint32(&a.flag, 0)
-	}
+	a.Store(value)
 }
 
 // String returns "true" or "false"
@@ -50,7 +48,9 @@ func (a *AtomicBoolean) UnmarshalJSON(b []byte) error {
 // AtomicInt64 is a helper struct to simulate atomic operations on an int64
 // Note that I could have used `type AtomicInt642 int64`, but I did not want to make it easy
 // to do + and - operations so easily without using atomic functions.
-type AtomicInt64 struct{ val int64 }
+type AtomicInt64 struct {
+	atomic.Int64
+}
 
 var _ json.Marshaler = &AtomicInt64{}
 var _ json.Unmarshaler = &AtomicInt64{}
@@ -72,7 +72,7 @@ func (a *AtomicInt64) UnmarshalJSON(b []byte) error {
 
 // Get the current int64
 func (a *AtomicInt64) Get() int64 {
-	return atomic.LoadInt64(&a.val)
+	return a.Load()
 }
 
 // String returns the integer as a string in a thread safe way
@@ -80,24 +80,9 @@ func (a *AtomicInt64) String() string {
 	return strconv.FormatInt(a.Get(), 10)
 }
 
-// Swap the current value with a new one
-func (a *AtomicInt64) Swap(newValue int64) int64 {
-	return atomic.SwapInt64(&a.val, newValue)
-}
-
-// Add a value to the current store
-func (a *AtomicInt64) Add(value int64) int64 {
-	return atomic.AddInt64(&a.val, value)
-}
-
 // Set the current store to a value
 func (a *AtomicInt64) Set(value int64) {
-	atomic.StoreInt64(&a.val, value)
-}
-
-// CompareAndSwap acts like a CompareAndSwap operation on the value
-func (a *AtomicInt64) CompareAndSwap(expected int64, newVal int64) bool {
-	return atomic.CompareAndSwapInt64(&a.val, expected, newVal)
+	a.Store(value)
 }
 
 // Duration returns the currently stored value as a time.Duration
